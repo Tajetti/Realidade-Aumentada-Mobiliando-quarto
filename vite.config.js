ca5f0292da53cf4d3c5daae5cdef33aaa28c347e
Tajetti <talhettialvaro@edu.unifil.br>
2025-11-26 13:42:02 -0300

    subindo projeto + feat: camera com menu

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import fs from 'fs'
+import path from 'path'
+export default defineConfig({
+server: {
+host: true, // expõe para a LAN
+port: 5173,
+https: {
+key: fs.readFileSync(path.resolve(__dirname, '192.168.1.101+1-key.pem')),
+cert: fs.readFileSync(path.resolve(__dirname, '192.168.1.101+1.pem'))
+}
+}
+})
